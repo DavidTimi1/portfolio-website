e37e4a25f31a1df3aeec57fbd1962e2125cfbecc
Dev_id <duwagbale07@gmail.com>
2026-02-10 00:06:00 +0100

    feat(projects): add projects section

---
+++ components/home/projects.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef, useState } from "react";
+import Link from "next/link";
+import { ArrowUpRight, Github, ExternalLink, Terminal, ListIcon } from "lucide-react";
+import { projects } from "@/data/projects";
+import { toast } from "sonner";
+import { ProjectItem } from "./project-item";
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+export default function Projects() {
+    const [currentProgress, setCurrentProgress] = useState(0);
+    const containerRef = useRef<HTMLDivElement>(null);
+    const { scrollXProgress } = useScroll({ container: containerRef });
+
+    scrollXProgress.updateAndNotify = setCurrentProgress;
+    const currentIndex = Math.round(currentProgress * 2);
+
+    // show projects in groups of 4s
+    const allProjectsSections = projects.reduce((acc, project, index) => {
+        if (index % 4 === 0) {
+            acc.push(projects.slice(index, index + 4));
+        }
+        return acc;
+    }, [] as typeof projects[]);
+
+    const handleDotClick = (idx: number) => {
+        if (idx > 1) {
+            console.log(scrollXProgress)
+        }
+    }
+
+
+    return (
+        <section id="featured-projects" className="min-h-screen snap-start w-full flex flex-col justify-center overflow-hidden relative">
+
+            <div className="w-full container flex flex-col space-y-5 pb-10">
+
+                {/* Header */}
+                <div className="w-full flex items-center justify-between">
+
+                    <div className="flex flex-col gap-2">
+                        <motion.h2
+                            initial={{ opacity: 0, x: -50 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            className="text-3xl md:text-4xl font-bold tracking-tighter"
+                        >
+                            FEATURED <span className="text-accent italic">PROJECTS</span>
+                        </motion.h2>
+                        <motion.p
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1 }}
+                            transition={{ delay: 0.2 }}
+                            className="text-zinc-400 max-w-xl text-sm"
+                        >
+                            A selection of my recent work. Swipe to explore more.
+                        </motion.p>
+                    </div>
+                    <div>
+                        <Button asChild>
+                            <Link href="/projects">
+                                <ListIcon className="w-5 h-5" />
+                                View All
+                            </Link>
+                        </Button>
+                    </div>
+                </div>
+
+                {/* Carousel Container */}
+                <div className="relative w-full space-y-3">
+                    <div
+                        ref={containerRef}
+                        className="w-full flex gap-6 overflow-y-hidden overflow-x-auto snap-x snap-mandatory scrollbar-hide"
+                    >
+                        {
+                            allProjectsSections.slice(0, 2).map((section, sectionIndex) => (
+                                <div key={sectionIndex} className="w-full grid snap-start grid-cols-1 md:grid-cols-2 shrink-0 gap-6">
+                                    {
+                                        section.map((project, index) => (
+                                            <ProjectItem
+                                                key={index}
+                                                project={project}
+                                                index={index}
+                                            />
+                                        ))
+                                    }
+                                </div>
+                            ))
+                        }
+
+                        {/* Humor / View All Card */}
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.9 }}
+                            whileInView={{ opacity: 1, scale: 1 }}
+                            className="min-w-[85vw] md:min-w-[400px] mx-20 snap-center flex items-center justify-center overflow-hidden"
+                        >
+                            <Link
+                                href="/projects"
+                                className="group relative h-full w-full bg-zinc-900 border border-zinc-800 border-dashed rounded-2xl flex flex-col items-center justify-center text-center hover:bg-zinc-800/30 hover:border-zinc-600 hover:scale-105 transition-all duration-300"
+                            >
+                                <div className="relative w-full max-h-64 bg-zinc-800">
+                                    <Image
+                                        alt="shy meme image"
+                                        width={200}
+                                        height={200}
+                                        src="/assets/shy-laugh-meme.jfif"
+                                        className="size-full object-top object-cover"
+                                    />
+                                </div>
+                                <div className="p-8">
+                                    <h3 className="text-2xl font-bold mb-2 text-zinc-100">
+                                        Whoa, that's a lot!
+                                    </h3>
+                                    <p className="text-zinc-400 max-w-xs mb-8">
+                                        I have way more cool stuff than I can fit in this carousel without breaking your scroll wheel.
+                                    </p>
+                                    <div className="flex items-center gap-2 text-blue-400 font-bold group-hover:gap-4 transition-all">
+                                        View Complete Archive <ArrowUpRight />
+                                    </div>
+
+                                </div>
+                            </Link>
+                        </motion.div>
+
+                        {/* {
+                            allProjectsSections.slice(2).map((section, sectionIndex) => (
+                                <div key={sectionIndex} className="w-full grid snap-start grid-cols-1 md:grid-cols-2 shrink-0 gap-6">
+                                    {
+                                        section.map((project, index) => (
+                                            <ProjectItem
+                                                key={project.id}
+                                                project={project}
+                                                index={index}
+                                            />
+                                        ))
+                                    }
+                                    {
+                                        sectionIndex === allProjectsSections.length - 1 && Array.from({ length: 4 - remainderCount }, (_, i) => (
+                                            <div className="h-64" key={i} />
+                                        ))
+                                    }
+                                </div>
+                            )
+                            )} */}
+                    </div>
+                </div>
+
+                {/* Navigation Dots */}
+                <div className="h-12 flex items-center justify-center gap-2 pb-4">
+                    {projects.map((_, idx) => (
+                        <button
+                            key={idx}
+                            onClick={() => handleDotClick(idx)}
+                            className={`transition-all duration-300 rounded-full 
+                            ${idx === currentIndex ? "w-8 h-1 bg-white" : "w-1 h-1 bg-zinc-700 hover:bg-zinc-500"}`}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
