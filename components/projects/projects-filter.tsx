b2f27a7356b79b016220c12dd3d13d85e9de608f
Dev_id <duwagbale07@gmail.com>
2026-02-18 16:35:46 +0100

    fix: replace use of `next/router` with `next/navigation`

--- components/projects/projects-filter.tsx
+++ components/projects/projects-filter.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { projects } from "@/data/projects";
 import { useEffect } from "react";
 import { useActiveSection } from "../providers/active-section-context";
