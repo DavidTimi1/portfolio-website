159647519ecc410626653b4bbdfa2b35c01ab0fc
Dev_id <duwagbale07@gmail.com>
2026-02-25 18:19:52 +0100

    chore: add react query to deps, utility function for getting project thumbnails, cleanup projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,6 +6,7 @@ import { ProjectsList } from "@/components/projects/project-list";
 import { ProjectsFilter } from "@/components/projects/projects-filter";
 import { Suspense } from "react";
 import Preloader from "../loading";
+import { ProjectHeader } from "@/components/projects/project-header";
 
 
 export default function ProjectsPage() {
@@ -14,27 +15,14 @@ export default function ProjectsPage() {
             <Preloader />
         }>
             <main className="min-h-screen w-full bg-zinc-950 text-foreground selection:bg-blue-500/30">
-                <GlassNav />
 
-                <div className="pb-20 pt-10 container">
+                <div className="py-10 container">
                     {/* Header */}
-                    <div className="mb-12">
-                        <Link href="/" className="inline-flex items-center gap-2 text-zinc-400 hover:text-foreground mb-8 transition-colors group">
-                            <ArrowLeftIcon className="group-hover:-translate-x-1 transition-transform" />
-                            Back to Home
-                        </Link>
-                        <div className="flex flex-col md:flex-row md:items-end justify-between gap-6">
-                            <div>
-                                <h2 className="text-3xl md:text-4xl font-bold tracking-tighter mb-4">
-                                    ALL <span className="text-accent italic">PROJECTS</span>
-                                    <span className="text-sm tracking-normal">{" "} ... well, most of em 😂 </span>
-                                </h2>
-                                <p className="text-zinc-400 max-w-2xl">
-                                    A comprehensive list of things I've built, broken, and fixed again.
-                                </p>
-                            </div>
+                    <ProjectHeader />
 
-                            {/* Search Input */}
+                    {/* Sticky Search & Filters */}
+                    <div className="sticky top-0 z-20 py-6 bg-zinc-950/80 backdrop-blur-md -mx-4 px-4">
+                        <div className="flex justify-end gap-6">
                             <ProjectsSearch />
                         </div>
 
